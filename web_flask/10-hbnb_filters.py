27c532e1e0f0d5f5126ce771c9d6068789e18879
Richard Sim <rdsim8589@gmail.com>
2017-03-29 12:15:39 -0700

    adding framework for 10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+this module starts a Flask Web application
+"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters/", strict_slashes=False)
+def states_hbnb(id):
+    """
+    route for /states/<id>
+
+    returns all states if no id
+    else returns all citys with the state
+
+    """
+    if id is "":
+        obj = storage.all("State").values()
+        return render_template("9-states.html",
+                               state=id,
+                               objs=obj)
+    else:
+        try:
+            obj = storage.all("State")[id]
+            state_name = obj.name
+            list_cities = obj.cities
+        except KeyError:
+            state_name = None
+            list_cities = None
+            pass
+
+        return render_template("9-states.html",
+                               state=state_name,
+                               objs=list_cities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
