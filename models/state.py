c6906c75ad17bd8aa5d2b97d90bfd23fafa2ea77
Richard Sim <rdsim8589@gmail.com>
2017-03-29 10:19:03 -0700

    added a getter function when env is set to fs

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel, Base, Table, Column, String
+import models
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 """
@@ -20,6 +21,17 @@ class State(BaseModel, Base):
                               cascade="all, delete, delete-orphan")
     else:
         name = ""
+        @property
+        def cities(self):
+            """
+            return a dictionary of the city and the city id
+            """
+            city_state_objs = []
+            for city_obj in models.storage.all("City").values():
+                if self.id == city_obj.state_id:
+                    city_state_objs.append(city_obj)
+            return city_state_objs
+
 
     def __init__(self, *args, **kwargs):
         """
